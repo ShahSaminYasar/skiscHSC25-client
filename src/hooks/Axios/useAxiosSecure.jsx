ee34c477e1ca1ac33cef779cb5c104fb0a042721
ShahSaminYasar <shahsaminyasarraihan@gmail.com>
2023-12-12 13:06:44 +0600

    Release: V1

--- src/hooks/Axios/useAxiosSecure.jsx
+++ src/hooks/Axios/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: "https://skischsc25-server.vercel.app/api/v1",
   withCredentials: true,
 });
 
